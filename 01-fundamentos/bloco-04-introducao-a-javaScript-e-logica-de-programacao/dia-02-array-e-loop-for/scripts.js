e51a1f26855b1b3d1d6e59467b25b76ea83fefe7
ViniciusTeixeiraFreitas <texera009@gmail.com>
2022-02-03 18:44:02 -0300

    add exercise 1

---
+++ 01-fundamentos/bloco-04-introducao-a-javaScript-e-logica-de-programacao/dia-02-array-e-loop-for/scripts.js
@@ -0,0 +1,24 @@
+// bloco 4.2
+//exercise 1//
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+  for (let index = 0 ;index < numbers.length; index += 1){
+    console.log(numbers[index]);
+  }
+
+//  exercise 2
+//   para realizar esse exercicio foi consultado o link: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
+//   O metodo reduce vai executar uma funcao que eu vou criar que vai ser aplicada em cada elemento de uma Array, e vai retornar em somente um valor. No caso, vai somar o elemento anterior com o atual e manter essa ordem somatoria para todos os elementos.
+
+
+const reducer = (previousValue, currentValue) => previousValue + currentValue;
+  console.log(numbers.reduce(reducer));
+  
+
+// exercise 3
+
+let sum = 0; 
+for (let index = 0 ;index < numbers.length; index += 1){
+   sum = numbers[index++];
+ } 
+  console.log(sum);
