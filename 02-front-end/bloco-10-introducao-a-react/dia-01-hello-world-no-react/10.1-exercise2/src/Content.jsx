7417a29a010d354173d6de38ecc98a10a223b2df
ViniciusTeixeiraFreitas <texera009@gmail.com>
2022-04-04 19:33:04 -0300

    adiciona os exercicios do dia 10.1

---
+++ 02-front-end/bloco-10-introducao-a-react/dia-01-hello-world-no-react/10.1-exercise2/src/Content.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+
+const conteudos = [
+  {
+    conteudo: 'High Order Functions',
+    bloco: 8,
+    status: 'Aprendido'
+  },
+  {
+    conteudo: 'Composicao de Componentes',
+    bloco: 11,
+    status: 'Aprendendo',
+  },
+  {
+    conteudo: 'Composicao de Estados',
+    bloco: 12,
+    status: 'Aprenderei'
+  },
+  {
+    conteudo: 'Redux',
+    bloco: 16,
+    status: 'Aprenderei'
+  },
+];
+
+class Content extends Component {
+  render() {
+    return (
+      <div className="content">
+        {conteudos.map((obj) => (
+          <div key={obj.conteudo} className='card'>
+            <h4>{`O conteudo é: ${obj.conteudo}`}</h4>
+            <p>{`status: ${obj.status}`}</p>
+            <p>bloco: {obj.bloco}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Content
